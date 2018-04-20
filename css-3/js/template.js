79a2acdef7bb866e197cf08924ab488922bb9acc
hyo950324 <38552458+hyo950324@users.noreply.github.com>
2018-04-21 03:08:29 +0900

    Add files via upload

---
+++ css-3/js/template.js
@@ -0,0 +1,9 @@
+jQuery(function($) {
+
+	/* ============== SHOW / HIDE GOOGLE MAP =============== */
+	$("#nino-map .nino-sectionHeading").click(function(){
+		$("#nino-map").toggleClass("showMap");
+		$(this).find(".text").toggle();
+	});
+
+});
